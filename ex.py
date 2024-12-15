e1f5938ea77f514ea6b61fbcd861f09711ca2c21
SanhyewNg <Sanhyew.Ng@gmail.com>
2024-12-15 11:16:21 -0500

    ..

---
+++ ex.py
@@ -0,0 +1,18 @@
+# Hi
+
+print("Hi")
+x='hh'
+y=2+3
+z='dsa'+'n bkj'
+
+if True or None:
+    pass
+
+def func():
+    return
+
+z=int(188)
+
+class My():
+    def __init__(self) -> None:
+        pass
